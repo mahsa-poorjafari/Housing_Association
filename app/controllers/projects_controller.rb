129310c8d31b2c8b3d5a1b8342b454639932a76f
mahsa <m_mw7462@yahoo.com>
2014-04-24 12:50:37 +0430

    add mailer user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :address, :description, :Area, :unit, :Status, :pictures_attributes)
+      params.require(:project).permit(:title, :address, :description, :Area, :unit, :Status, pictures_attributes: [:image])
     end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "web.development@raush.ir"
   
   def send_user_mail
-    mail(to: 'mahsa.poorjafari@gmail.com', subject: 'اتحادیه مسکن')
+    mail(to: 'mahsa.poorjafari@gmail.com', subject: 'house')
   end
   
 end
