d7bfa48fbc43ff0967d274b8ae4d94b9f00103db
mahsa <m_mw7462@yahoo.com>
2014-07-17 16:12:56 +0430

    add validate for mobile field on Partnership

--- app/models/partnership.rb
+++ app/models/partnership.rb
@@ -4,4 +4,5 @@ class Partnership < ActiveRecord::Base
   validates :first_name, :last_name, :email, :father_name, :mobile, :presence => {:message => 'فیلد های ستاره دار را پر کنید'}
   validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => 'ایمیل نامعتبر است.' } 
   validates :email, :uniqueness => {:message => 'ایمیل در سیستم وجود دارد'}
+  validates :mobile, format: { with: /\d{10}/, message: "لطفا ده رقم شماره تلفن همراه خود را بدون صفر وارد کنید." }
 end
